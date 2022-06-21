7df02b6a657c73287704fec89c7d8f2f9c038c4e
chen <1005518455@qq.com>
2022-06-21 18:03:23 +0800

    整理项目结构

---
+++ src/test/java/sut/edu/c601/dormitory/manage/DmApplicationTests.java
@@ -0,0 +1,13 @@
+package sut.edu.c601.dormitory.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
